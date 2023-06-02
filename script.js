8e692f9ea55bea1b0d6b242268321408ea44e2e8
Anisha Ramnani <anisha.ramnani@gmail.com>
2023-06-02 16:24:41 -0400

    a work in progress

--- script.js
+++ script.js
@@ -20,6 +20,16 @@ const changeColor = () => {
     if (shouldUpdate) {
       body.classList.remove(body.classList[0]);
       body.classList.add(`color-${panel.dataset.color}`);
+      const fragment = document.createDocumentFragment();
+      const elemDiv = document.createElement('div');
+      elemDiv.classList.add('panel');
+      elemDiv.setAttribute('data-color', 'green');
+      fragment.appendChild(elemDiv);
+      const wrapper = document.querySelector('.wrapper');
+      wrapper.appendChild(fragment);
     }
   });
 }
+
+// theory: where we put this document div implementation
+// move adding the div outside of shouldupdate but where or how?
